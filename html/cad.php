95c009ef121b183d022bb2e7ae31e87c67538e8a
Hiagoprogramer_play <161398837+hiagoprogramerplay@users.noreply.github.com>
2024-05-16 20:43:07 -0300

    preenchend formularioos de cadastro

---
+++ html/cad.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html5>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA_Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CADASTRO DOS ALUNOS</title>
+    <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+</head>
+<body>   
+    <H1>
+        <?php
+
+        
+        ?>
+    </H1>
+</body>
+</html>
